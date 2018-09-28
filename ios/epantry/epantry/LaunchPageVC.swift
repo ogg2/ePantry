d07f4d1127f31761b8474a37a1c8f6278291b51f
Caleb Cain <cjc166@case.edu>
2018-09-28 12:19:43 -0400

    iosApp initialized, Launch Page created

---
+++ ios/epantry/epantry/LaunchPageVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  epantry
+//
+//  Created by Caleb Cain on 9/28/18.
+//  Copyright © 2018 Caleb. All rights reserved.
+//
+
+import UIKit
+
+class LaunchPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBOutlet weak var pantryButton: UIButton!
+    @IBOutlet weak var recipeButton: UIButton!
+    @IBOutlet weak var groceryListButton: UIButton!
+
+
+}
+
