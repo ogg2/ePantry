40f76b421184252ba4daf40b15bd30b9eb717c96
smedlang <smedlang@gmail.com>
2018-10-13 13:55:48 -0400

    login, registration

--- backend/app.js
+++ backend/app.js
@@ -10,18 +10,66 @@ mongoose.connect(process.env.MONGODB_URI);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-let models = require('./models.js');
+let User = require('./models.js').User;
 
 
-/* returns the userid */
+/* returns the userid
+{
+input:
+username: username,
+password: password 
+
+output:
+{
+userid: userid
+}}*/
 app.post("/login", (req, res)=> {
+  let username = req.body.username;
+  let password = req.body.password;
 
-  res.json({userid: ""});
+  User.findOne({
+    username: username,
+    password: password
+  }).then (result => {
+    res.json({userid: result._id});
+  }).catch(err => {
+    res.status({status: 400});
+  })
 });
 
-app.post("/register", (req, res) => {
-  res.json({});
 
+/* input: {
+username: username,
+password: password }
+
+output
+{
+status: 200
+}*/
+app.post("/register", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+//if this username is taken
+  User.findOne ({username: username})
+  .then(result => {
+    console.log("username taken");
+    res.json({status: 400});
+  }).catch(err=> {
+    //add the user
+    let newUser = new User({
+      username: username,
+      password: password
+    });
+    newUser.save()
+    .then(()=> {
+      res.json({status:200});
+    })
+    .catch(err=> {
+      console.log("problem saving user");
+      res.json({status: 400});
+    })
+  })
 });
 
 app.get("/groceryList/:userid", (req,res)=> {
