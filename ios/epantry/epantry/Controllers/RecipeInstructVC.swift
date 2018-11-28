edcdad80334306debd09f95ff3e5da1afe9935a8
Owen Gibson <Owen@Owens-MacBook-Pro.local>
2018-11-27 22:10:11 -0500

    Scroll view for ingredients and recipe instructions

--- ios/epantry/epantry/Controllers/RecipeInstructVC.swift
+++ ios/epantry/epantry/Controllers/RecipeInstructVC.swift
@@ -9,13 +9,26 @@
 import Foundation
 import UIKit
 
+//UITableViewDelegate, UITableViewDataSource
+
 class RecipeInstructVC: UIViewController {
+    
+    
+    @IBOutlet weak var scrollViewIngredients: UIScrollView!
+    @IBOutlet weak var scrollViewInstructions: UIScrollView!
+    @IBOutlet weak var instructions: UILabel!
+    @IBOutlet weak var ingredients: UILabel!
+    
+    let listofIngredients = ["1 Cup of Milk", "1 T of Pepper"]
+    
+    var ingredientsList: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        scrollViewInstructions.contentLayoutGuide.bottomAnchor.constraint(equalTo: instructions.bottomAnchor).isActive = true
+        scrollViewIngredients.contentLayoutGuide.bottomAnchor.constraint(equalTo: ingredients.bottomAnchor).isActive = true
+        
         print("Recipe Instructions Loaded")
     }
-    
-    
 }
